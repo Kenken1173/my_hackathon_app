9eed12b5af7db18460cf75524ac4b291c8332e41
KenshinKaraage <iedenetai@gmail.com>
2025-08-08 11:36:17 +0900

    create login window

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,15 @@ use App\Http\Controllers\MilestonesWBSController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MilestoneListController;
 use App\Http\Controllers\UserGoalListController;
+
+use App\Http\Controllers\LoginController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserGoalListController::class, "index"]); // ユーザーごとのゴール一覧画面
 Route::get('/list/{goal_id}', [MilestoneListController::class, "get"]); // ユーザーのゴールごとのマイルストーン一覧画面
 Route::get('/table/{goal_id}', [MileStoneTableController::class, "get"]); // ユーザーのゴールごとのマイルストーン一覧画面
 Route::get('/milestones-wbs/{goal_id}', [MilestonesWBSController::class, "get"]);
+Route::get('/login', [LoginController::class, "get"]);
+
 
