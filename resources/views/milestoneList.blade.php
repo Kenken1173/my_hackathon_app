0d6aba0d9df60d1b2d2ea1d82dabd3a6477dff8c
MatsubaraYuki <c3p31102@bunkyo.ac.jp>
2025-08-08 12:57:26 +0900

    DB接続。掃除

--- resources/views/milestoneList.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout :username=$username title="Milestone list">
-    <a>{{ $goal["name"] }}</a>
-    @foreach ($goal["milestones"] as $milestone)
-        <p> {{  $milestone["name"] }} </p>
-    @endforeach
-
-</x-layout>
